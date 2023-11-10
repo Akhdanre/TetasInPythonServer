390c344416389a3a5f5012ad8ef631ca60acc981
Akhdanre <danakhdan12@gmail.com>
2023-11-10 08:39:27 +0700

    add delete data in user service and add somoe adjustment in testing

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -8,6 +8,7 @@ sys.path.append("..")
 from app.model import models
 from app.schema.register_request import RegisterRequest
 from app.schema.web_response_schema import WebResponse
+from app.database.dbConfig import session_scope
 
 class User:
     def register(user: RegisterRequest, db: Session):
@@ -31,3 +32,12 @@ class User:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=str(e)
             )
+
+    def deleteAllUsers():
+        """delete all users data for testing, table must null for check"""
+        with session_scope() as db:
+            db_user_count = db.query(models.UserModel).delete()
+            if db_user_count > 0:
+                db.commit()
+                return True
+            return False
--- test/users_test.py
+++ test/users_test.py
@@ -1,10 +1,17 @@
 from fastapi.testclient import TestClient
+from fastapi import Depends
 from app.main import app
+from app.service.user_service import User
+from sqlalchemy.orm import Session
+from app.utils.deps import get_db
+
 
 client = TestClient(app)
 
 
 def test_register_succes():
+    deleteStatus = User.deleteAllUsers()
+
     response = client.post(
         "/api/register",
         json={
