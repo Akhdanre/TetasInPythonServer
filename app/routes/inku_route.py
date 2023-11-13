1c21959559a5a2cd95308f0af72ad832b72a4557
Akhdanre <danakhdan12@gmail.com>
2023-11-13 14:08:09 +0700

    fix sse stream event

--- app/routes/inku_route.py
+++ app/routes/inku_route.py
@@ -12,9 +12,8 @@ routeInku = APIRouter()
 
 @routeInku.get('/sse/control/temp/{user_id}/{token}')
 async def message_stream(request: Request, user_id: str, token: str):
-
     return EventSourceResponse(
-        InkuStreamService.event_generator(request, user_id, token),
+        InkuStreamService().event_generator(request, user_id, token),
         ping=60,
         ping_message_factory=lambda: ServerSentEvent({"ping": datetime.today().strftime('%Y-%m-%d %H:%M:%S')}))
 
